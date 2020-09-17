523f6720c9986f74f6fcb1eba641c74ac472c02a
jessicasalbert <jessicarsalbert@gmail.com>
2020-09-17 17:00:21 -0400

    complete

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -28,7 +28,7 @@ class EmployeesController < ApplicationController
 
     private 
     def employee_params
-        params.require(:employee).permit(:first_name, :last_name, :title, :office, :alias, :dog, :img_url)
+        params.require(:employee).permit(:first_name, :last_name, :title, :office, :alias, :dog_id, :img_url)
     end
 
 
